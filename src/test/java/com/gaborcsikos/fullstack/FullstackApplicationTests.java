7b7e1e0ab2648914aabb5c8e94917ce0ef17e6bd
gcsikos <csiki123@gmail.com>
2020-11-27 14:25:23 +0100

    initial commit

---
+++ src/test/java/com/gaborcsikos/fullstack/FullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaborcsikos.fullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
